3335e13bfd0929bf6f6d6d5f3080e01ebc30a65d
Girish <girsh87@gmail.com>
2021-08-30 23:41:16 +0530

    Added new first java file

---
+++ TestV1/src/com/FirstJavaFile.java
@@ -0,0 +1,12 @@
+package com;
+
+public class FirstJavaFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello GIT...");
+
+	}
+
+}
